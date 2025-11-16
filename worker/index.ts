0f185fd3d63ae4cd88f19b7996084be7e56c341b
lc <lichon@foxmail.com>
2025-11-16 18:46:27 +0800

    init vite env

--- worker/index.ts
+++ worker/index.ts
@@ -1,12 +1,14 @@
-export default {
-  fetch(request) {
-    const url = new URL(request.url);
+import { Hono } from 'hono'
 
-    if (url.pathname.startsWith("/api/")) {
-      return Response.json({
-        name: "Cloudflare",
-      });
-    }
-		return new Response(null, { status: 404 });
-  },
-} satisfies ExportedHandler<Env>;
+type Bindings = {
+  LOCAL_DEBUG: boolean
+}
+
+const app = new Hono<{ Bindings: Bindings }>()
+
+app.all('/api/*', async (c) => {
+  // TODO: implement some apis
+  return c.json({}, 404)
+})
+
+export default app
